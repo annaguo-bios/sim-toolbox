7d8ef38d71b38c2d89fc8472f207f22d239a2058
Anna Guo <apple@Annas-Mac-mini.local>
2025-12-17 22:58:42 -0500

    update submit to specify start and end idx

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,19 +1,24 @@
 module load python/3.8
 module load R/4.2.2
 dsq_path="/home/aguo28/dSQ-master"
+
 ##########################
 # This is step1_dsq.sh
 ##########################
 
-echo "Enter jobfile prefix"
+echo "Enter jobfile prefix. For example, prefix of joblist_n1.txt is joblist"
 read prefix
 
-echo "Enter the number of jobs to run"
+echo "Enter the start number of jobs to run. Default is 1"
 read n
+n=${n:-1}
+
+echo "Enter the end number of jobs to run"
+read m
 
 ##########################
 # This is step3_submit.sh
 ##########################
-for i in $(seq 1 ${n}); do
+for i in $(seq ${n} ${m}); do
   sbatch ${prefix}_n$i.sh
 done
